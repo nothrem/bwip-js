17c41961e0e35b7c5ad0caa4987b49953e4748fb
radek_imax <git@nothrem.cz>
2017-03-07 17:16:13 +0100

    + prevent browser from freezing when generating multiple codes at once

--- jquery.bwip.js
+++ jquery.bwip.js
@@ -15,7 +15,7 @@
  *  $.bwip({color: 'transparent', scale: 2, title: 'Human-readable text');
  *
  *  To set a callback for the barcode conversion use:
- *  A) $('#barcode').bwip(callback); //just pass a function as callback
+ *  A) $('#barcode').bwip(callback); //just pass a function as callback; this presumes all other values being set using $.bwip() and/or HTML attributes
  *  B) $('#barcode').bwip({callback:callback}); //include the callback in options
  *  C) $.bwip({callback:callback}); //set default callback (not called in A) or B) case)
  *  D) $('#barcode').on('bwipdone', callback); //Use event handler for 'bwipdone' or 'bwiperror'
@@ -36,7 +36,7 @@
  *  	classname: 'class',  //CSS class for the image element; always adds class bwipCode
  *  	text: 'string',      //set human-readable text for the barcode or include code if set to True; by default is True
  *  	title: false,        //if true, it will add title attribute for the image
- *  	color: 'transparent',//set background color of the barcode, by default is transparent
+ *  	color: 'fff',        //set background color of the barcode, give as hexadecimal code, by default is white ('fff')
  *  	scale: 2,            //set scale of the image as a number or an object with x and y properties, by default is 2
  *      padding: 0,          //set the image padding as a number or an object with x and y properties, by default is 0
  *  };
@@ -46,10 +46,32 @@
  *  the plugin will automatically detect the folder and use it. Using root option
  *  can also make the first conversion faster if your HTML contains lots of script tags.
  *
+ *  The $().bwip() method automatically prevents browser from freezing (becoming unresponsive)
+ *  while generating multiple codes at the same time. Codes will be generated in sequence
+ *  and appended into the page progressively.
+ *  If this is unwanted, you can use the cache described below. The method
+ *  will trigger event "bwipalldone" on the window when finished.
+ *
+ *  The $().bwip() method uses cache for already generated codes
+ *  (based on code type, code value and options; Options root, mode, id
+ *  and classname does not affect the caching).
+ *  You can use mode:'none' option to prepare the code in the background
+ *  and then displayed it by using other mode or a callback.
+
+	//when all codes are prepared, display them
+	$(window).on('bwipalldone', function() {
+		$('.__bwip_done').bwip();
+	});
+
+	//prepare all codes but do not display them
+	$('.bwip-codes').bwip({mode:'none'});
+ *
  * @copy Nothrem Sinsky (c) 2016
  */
 ;(function(window, $) {
 	var
+		//constants
+		PROCESS_LIST_INTERVAL = 1, //1ms beween each code generation
 		//internal reference to BWIPJS and BWIPP libraries (can be reused)
 		bwipp = false,
 		bwipjs = false,
@@ -64,11 +86,14 @@
 		],
 		//private properties
 		mainProcess,
+		processList,
+		codeCache,
 		//private methods
 		getRoot,
 		load,
 		getScript,
-		error;
+		getCode,
+		generateCode;
 	//var
 
 	//Private methods
@@ -146,6 +171,87 @@
 		});
 	};
 
+	getCode = function(type, code, options) {
+		var json = JSON.stringify(options), process;
+
+		codeCache = codeCache || [];
+		codeCache[type] = codeCache[type] || [];
+		codeCache[type][code] = codeCache[type][code] || [];
+		return codeCache[type][code][json] = codeCache[type][code][json] || $.Deferred(function() {
+				var data = {
+						process: this,
+						type: type,
+						code: code,
+						options: options
+					};
+
+				if (processList) {
+					processList.push(data);
+					return;
+				}
+
+				processList = [data];
+				setTimeout(generateCode, PROCESS_LIST_INTERVAL);
+			})
+		;
+	}
+
+	generateCode = function() {
+		if (!processList) {
+			return;
+		}
+		var
+			data = processList.shift(),
+			options = data.options,
+			bwip = new bwipjs(window.Module, false);
+
+		bwip.bitmap(new Bitmap(options.color || 'FFF'));
+		if (options.scale) {
+			if (options.scale.x || options.scale.y) {
+				bwip.scale(options.scale.x||2, options.scale.y||2);
+			}
+			else {
+				bwip.scale(options.scale, options.scale);
+			}
+		}
+		else {
+			bwip.scale(2, 2);
+		}
+		if (options.padding) {
+			if (options.padding.x || options.padding.y) {
+				bwip.bitmap().pad(options.padding.x||0, options.padding.y||0);
+			}
+			else {
+				bwip.bitmap().pad(options.padding, options.padding);
+			}
+		}
+		else {
+			bwip.bitmap().pad(0, 0);
+		}
+
+		delete options.scale;
+		delete options.padding;
+
+		try {
+			bwipp(bwip, data.type, data.code, options);
+		}
+		catch (e) {
+			data.process.reject(e);
+		}
+
+		var canvas = document.createElement('canvas');
+		bwip.bitmap().show(canvas, 'N');
+		data.process.resolve(canvas.toDataURL());
+
+		if (processList.length) { //there are more codes to be converted...
+			setTimeout(generateCode, PROCESS_LIST_INTERVAL);
+		}
+		else { //no more codes, stop processing
+			processList = false;
+			$(window).trigger('bwipalldone');
+		}
+	};
+
 	//jQuery methods
 	$.bwip = function(options) {
 		if (options) {
@@ -154,7 +260,7 @@
 			}
 		}
 		return $;
-	}
+	};
 
 	$.fn.bwip = function(options) {
 		if ($.isFunction(options)) {
@@ -176,7 +282,6 @@
 					code = el.text().trim(),
 					title = el.attr('title'),
 					data = el.data('barcode'),
-					bwip = new bwipjs(window.Module, false),
 					config = {};
 
 				el.addClass('__bwip_processing');
@@ -191,7 +296,7 @@
 					title = options.text;
 				}
 
-				config.includetext = options.text !== false;
+				config.includetext = (options.text !== false);
 				if (title) {
 					config.alttext = title;
 				}
@@ -200,41 +305,16 @@
 					config.eclevel = options.eclevel;
 				}
 
-				bwip.bitmap(new Bitmap(options.color || 'FFF'));
-				if (options.scale) {
-					if (options.scale.x || options.scale.y) {
-						bwip.scale(options.scale.x||2, options.scale.y||2);
-					}
-					else {
-						bwip.scale(options.scale, options.scale);
-					}
-				}
-				else {
-					bwip.scale(2, 2);
-				}
-				if (options.padding) {
-					if (options.padding.x || options.padding.y) {
-						bwip.bitmap().pad(options.padding.x||0, options.padding.y||0);
-					}
-					else {
-						bwip.bitmap().pad(options.padding, options.padding);
-					}
-				}
-				else {
-					bwip.bitmap().pad(0, 0);
-				}
+				config.scale = options.scale;
+				config.padding = options.padding;
 
-				try {
-					bwipp(bwip, type, code, config);
-				}
-				catch (e) {
+				getCode(type, code, config)
+				.fail(function(e) {
 					el.trigger('bwiperror', e);
 					throw (e);
-					return;
-				}
-
+				})
+				.done(function(code) {
 					var
-						canvas = document.createElement('canvas'),
 						image = $('<img>');
 
 					image
@@ -251,8 +331,8 @@
 						image.attr('title', code);
 					}
 
-					bwip.bitmap().show(canvas, 'N');
-					image.attr('src', canvas.toDataURL());
+
+					image.attr('src', code);
 
 					if ('string' === typeof options.mode) {
 						options.mode = options.mode.toLowerCase();
@@ -280,9 +360,10 @@
 					if ($.isFunction(options.callback)) {
 						options.callback.call(el, image);
 					}
-					el.addClass('__bwip_done');
+					el.addClass('__bwip_done').removeClass('__bwip_processing');
 					el.trigger('bwipdone', image);
-				});
+				}); //getCode.done()
+			}); //each element from this jquery object
 		}).fail(function(e) {me.trigger('bwiperror', e)});
 
 		return this;
